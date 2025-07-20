b22c51847dacc0b742815ae8334aded3c8dd9422
ReyanNaseem <reyannaseem516@gmail.com>
2025-07-20 18:26:57 +0500

    link with backend url

--- frontend/src/pages/ApplyLoan.jsx
+++ frontend/src/pages/ApplyLoan.jsx
@@ -71,7 +71,7 @@ const ApplyLoan = () => {
             <input
               type="text"
               name="fullName"
-              // required
+              required
               value={form.fullName}
               onChange={handleChange}
               className="w-full border border-gray-300 rounded-md px-3 py-2 mt-1"
